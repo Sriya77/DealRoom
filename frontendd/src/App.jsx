82fbb3e3c00de44e77ba62475c496df2f07c695f
Sriya Chakinala <chakinalarohan95@gmail.com>
2025-04-17 17:11:22 +0530

    Connected frontend and backend

--- frontendd/src/App.jsx
+++ frontendd/src/App.jsx
@@ -1,14 +1,33 @@
 
 import { Route, Routes } from 'react-router-dom'
-import './App.css'
+// import './App.css'
 import NavBar from './components/NavBar'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { useAuthStore } from './store/useAuthStore'
+import { useEffect } from 'react'
+
+import { Loader } from "lucide-react";
 
 function App() {
+
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+
+  useEffect(()=>{
+    checkAuth()
+  }, [checkAuth]);
+
+  console.log({authUser})
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
 
   return (
     <>
