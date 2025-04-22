cd540440b3f1ec5a80fbacf728a768436145cc91
Sriya Chakinala <chakinalarohan95@gmail.com>
2025-04-22 18:01:31 +0530

    deployment issue changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import cors from "cors";
-
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
-
-
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 import path from "path";
@@ -23,8 +20,17 @@ app.use(cors({
     credentials: true
 }))
 
+// app.use("/api/auth", authRoutes)
+// app.use("/api/messages", messageRoutes)
+
+console.log("Before /api/auth route");
 app.use("/api/auth", authRoutes)
+
+console.log("Before /api/messages route");
 app.use("/api/messages", messageRoutes)
+
+console.log("Finished setting routes");
+
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
